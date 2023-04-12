f41283806c93deb8c7705e8bb11c96cd80edeb7c
a22natth <a22natth@student.his.se>
2023-04-12 11:17:27 +0200

    private member variabel skapad

---
+++ app/src/main/java/com/example/webviewapp/WebView.java
@@ -0,0 +1,24 @@
+package com.example.webviewapp;
+
+public class WebView {
+    private String myWebView;
+
+    public WebView (){
+        myWebView="webview";
+    }
+
+    public WebView (String m ){
+        myWebView=m;
+    }
+
+    public void setMyWebView(String m){
+        myWebView=m;
+    }
+
+    public String getMyWebView(){
+        return myWebView;
+    }
+
+}
+
+
